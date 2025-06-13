c6a59e017557f94595c51c7621943689d5e8d659
sghosh31 <sghosh31@LIN-D0NLBK3>
2025-06-13 15:14:19 +0530

    updated in Entity class

--- HotelService/src/main/java/com/project/hotel/entity/Hotel.java
+++ HotelService/src/main/java/com/project/hotel/entity/Hotel.java
@@ -1,9 +1,6 @@
 package com.project.hotel.entity;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
